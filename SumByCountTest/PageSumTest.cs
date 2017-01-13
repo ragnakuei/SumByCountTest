d0db0e4aa4910481dd6cfb43adf201d13abd37d9
Kuei-PC\Kuei <ragnakuei@livemail.tw>
2017-01-13 16:07:52 +0800

    待思考不用 Order class 的做法

--- SumByCountTest/PageSumTest.cs
+++ SumByCountTest/PageSumTest.cs
@@ -1,6 +1,7 @@
 ﻿using ExpectedObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SumByCount;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SumByCountTest
@@ -8,12 +9,26 @@ namespace SumByCountTest
     [TestClass]
     public class PageSumTest
     {
+        private List<Order> _orders = new List<Order>(){
+                new Order { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 },
+                new Order { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 },
+                new Order { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 },
+                new Order { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 },
+                new Order { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 },
+                new Order { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 },
+                new Order { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 },
+                new Order { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 },
+                new Order { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 },
+                new Order { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 },
+                new Order { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 }
+            };
+
         [TestMethod]
         public void SumByN_傳入一組Cost_3筆一組總合應為_6_15_24_21()
         {
             //arrange
             PageSum target = new PageSum();
-            int[] ordersCost = Enumerable.Range(1, 11).ToArray();
+            int[] ordersCost = _orders.Select(o => o.Cost).ToArray();
             int[] expected = new int[] { 6, 15, 24, 21 };
 
             //act
@@ -28,7 +43,7 @@ namespace SumByCountTest
         {
             //arrange
             PageSum target = new PageSum();
-            int[] ordersRevenue = Enumerable.Range(11, 11).ToArray();
+            int[] ordersRevenue = _orders.Select(o => o.Revenue).ToArray();
             int[] expected = new int[] { 50, 66, 60 };
 
             //act
@@ -37,5 +52,13 @@ namespace SumByCountTest
             //assert
             expected.ToExpectedObject().ShouldEqual(actual);
         }
+    }
+
+    internal class Order
+    {
+        public int Id { get; set; }
+        public int Cost { get; set; }
+        public int Revenue { get; set; }
+        public int SellPrice { get; set; }
     }
 }
